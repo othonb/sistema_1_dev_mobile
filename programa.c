96606d2d20505c6c322fb410eb0a6e8c47b457b0
Othon Batista <othonbatista@gmail.com>
2022-03-18 20:45:52 -0300

    Função soma dois inteiros

--- programa.c
+++ programa.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 float somaDoisFloats(float, float);
+int somaDoisInteiros(int, int);
 
 int main(int argc, char **argv) {
 
@@ -9,10 +10,17 @@ int main(int argc, char **argv) {
 
   printf("Soma(%f, %f) = %f\n", 2.5f, 3.5f, somaDoisFloats(2.5f, 3.5f));
 
+  printf("Soma(%d, %d) = %d\n", 2, 3, somaDoisInteiros(2, 3));
+
   return 0;
 }
 
 float somaDoisFloats(float a, float b) {
+
+  return a + b;
+}
+
+int somaDoisInteiros(int a, int b) {
 
   return a + b;
 }
