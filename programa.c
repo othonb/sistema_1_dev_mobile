eeaa0ed1bc42e3cc61372cad62af2e99d4e0dc9a
Othon Batista <othonbatista@gmail.com>
2022-03-18 20:34:18 -0300

    Função de somar dois floats

--- programa.c
+++ programa.c
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+float somaDoisFloats(float, float);
+
 int main(int argc, char **argv) {
 
   printf("Hello World!\n");
 
+  printf("Soma(%f, %f) = %f\n", 2.5f, 3.5f, somaDoisFloats(2.5f, 3.5f));
+
   return 0;
+}
+
+float somaDoisFloats(float a, float b) {
+
+  return a + b;
 }
