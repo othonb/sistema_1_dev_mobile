4276a529e5067c1e671257f2139f18416b0c35f0
Othon Batista <othonbatista@gmail.com>
2022-03-18 21:01:13 -0300

    Subtrai dois inteiros

--- programa.c
+++ programa.c
@@ -11,6 +11,11 @@ int somaDoisInteiros(int a, int b) {
   return a + b;
 }
 
+int subtraiDoisInteiros(int a, int b) {
+
+  return a - b;
+}
+
 
 int main(int argc, char **argv) {
 
@@ -19,6 +24,8 @@ int main(int argc, char **argv) {
   printf("Soma(%f, %f) = %f\n", 2.5f, 3.5f, somaDoisFloats(2.5f, 3.5f));
 
   printf("Soma(%d, %d) = %d\n", 2, 3, somaDoisInteiros(2, 3));
+
+  printf("Subtrai(%d, %d) = %d\n", 2, 3, subtraiDoisInteiros(2, 3));
 
   return 0;
 }
